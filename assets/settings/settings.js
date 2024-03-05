cd77c8995d46ea1ed13397093d75c95f14c630ae
VladimirGorin <vladimirgorin410@gmail.com>
2024-03-05 23:39:47 +0300

    f

---
+++ assets/settings/settings.js
@@ -0,0 +1,10 @@
+module.exports.oauth_token = "6da66d21d07347678e947ee679d5bda2"
+module.exports.counterId = 92059143
+module.exports.token_test = "6582380572:AAFB-IS1uTcosUz2T-8R18hfRrgvyeOku7A"
+module.exports.token = "6890721718:AAHb8fKXRIuhQz1FcBHB5VAVotUN84Emq78"
+module.exports.VONAGE_API_KEY = "4fa85503"
+module.exports.VONAGE_API_SECRET  = "5wmd4tLeXGHWcRuL"
+module.exports.port = 8000
+
+module.exports.email_port = 465
+module.exports.email_host = "smtp.hostinger.com"

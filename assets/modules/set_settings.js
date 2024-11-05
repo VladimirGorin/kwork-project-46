83bdd7f699ec27b89e7be0e6da66352074b1d3ee
VladimirGorin <vladimirgorin410@gmail.com>
2024-11-06 00:13:31 +0600

    fix bugs

--- assets/modules/set_settings.js
+++ assets/modules/set_settings.js
@@ -3,69 +3,54 @@ const fs = require("fs");
 
 function findAndDeleteById(arr, id) {
   const index = arr.findIndex(item => item.no === id);
-
   if (index !== -1) {
-    arr.splice(index);
+    arr.splice(index, 1); // Remove the found item
   }
+  return arr;
+}
 
-  return arr
+function flattenArray(arr) {
+  return arr.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      acc.push(...flattenArray(item)); // Recursively flatten
+    } else {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports.set_settings = (link, chatId, bot, step, site) => {
   let pathToFolder = `./assets/data/sites/${site}/`;
-  const oldSettings = JSON.parse(
-    fs.readFileSync(`${pathToFolder}settings.json`)
-  );
-
-
-  const oldTransactionsData = JSON.parse(
-    fs.readFileSync(`${pathToFolder}custom_transactions_settings.json`)
-  );
-
+  const oldSettings = JSON.parse(fs.readFileSync(`${pathToFolder}settings.json`));
+  const oldTransactionsData = JSON.parse(fs.readFileSync(`${pathToFolder}custom_transactions_settings.json`));
 
   fs.access(`../data/sites/${site}/`, function (error) {
     if (error) {
       switch (step) {
         case "qr":
-          let message1 = `Excellent! The qr has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}qr_settings.json`,
-            JSON.stringify({ qr_code_link: link }, null, "\t")
-          );
-
-          bot.sendMessage(chatId, message1);
+          fs.writeFileSync(`${pathToFolder}qr_settings.json`, JSON.stringify({ qr_code_link: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The qr has been saved in site`);
           break;
+
         case "qr_deposit":
-          let qr_deposit_message = `Excellent! The qr deposit has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}qr_deposit_settings.json`,
-            JSON.stringify({ qr_code_link: link }, null, "\t")
-          );
+          fs.writeFileSync(`${pathToFolder}qr_deposit_settings.json`, JSON.stringify({ qr_code_link: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The qr deposit has been saved in site`);
+          break;
 
         case "bitcoin_address":
-          let bitcoin_address_message = `Excellent! The bitcoin address has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}bitcoin_address.json`,
-            JSON.stringify({ bitcoin_address: link }, null, "\t")
-          );
-
-          bot.sendMessage(chatId, bitcoin_address_message);
+          fs.writeFileSync(`${pathToFolder}bitcoin_address.json`, JSON.stringify({ bitcoin_address: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The bitcoin address has been saved in site`);
           break;
-        case "full_wallet_address":
-          let full_wallet_address_message = `Excellent! The full wallet address has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}full_wallet_address_settings.json`,
-            JSON.stringify({ full_wallet_address: link }, null, "\t")
-          );
 
-          bot.sendMessage(chatId, full_wallet_address_message);
+        case "full_wallet_address":
+          fs.writeFileSync(`${pathToFolder}full_wallet_address_settings.json`, JSON.stringify({ full_wallet_address: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The full wallet address has been saved in site`);
           break;
 
         case "custom_transactions_settings":
           let custom_transactions_message = `Excellent! The transaction data has been saved in site`;
-
           if (oldTransactionsData && oldTransactionsData.length) {
-            console.log("here")
             fs.writeFileSync(
               `${pathToFolder}custom_transactions_settings.json`,
               JSON.stringify([...oldTransactionsData, link], null, "\t")
@@ -76,121 +61,87 @@ module.exports.set_settings = (link, chatId, bot, step, site) => {
               JSON.stringify([link], null, "\t")
             );
           }
-
           bot.sendMessage(chatId, custom_transactions_message);
           break;
 
         case "custom_transactions":
           let custom_transactions_message_ = `Excellent! The transactions data has been saved in site`;
-
+          const flattenedLink = Array.isArray(link) ? flattenArray(link) : [link];
           if (oldTransactionsData && oldTransactionsData.length) {
             fs.writeFileSync(
               `${pathToFolder}custom_transactions_settings.json`,
-              JSON.stringify([...oldTransactionsData, link], null, "\t")
+              JSON.stringify([...oldTransactionsData, ...flattenedLink], null, "\t")
             );
           } else {
             fs.writeFileSync(
               `${pathToFolder}custom_transactions_settings.json`,
-              JSON.stringify([link], null, "\t")
+              JSON.stringify(flattenedLink, null, "\t")
             );
           }
-
           bot.sendMessage(chatId, custom_transactions_message_);
           break;
 
         case "delete_custom_transactions_settings":
           let delete_custom_transactions_message = `Excellent! The transaction data has been deleted from the site`;
-
-          const newTransactionsArray = findAndDeleteById(oldTransactionsData, link)
+          const newTransactionsArray = findAndDeleteById(oldTransactionsData, link);
           fs.writeFileSync(
             `${pathToFolder}custom_transactions_settings.json`,
             JSON.stringify(newTransactionsArray, null, "\t")
           );
-
           bot.sendMessage(chatId, delete_custom_transactions_message);
           break;
 
         case "price":
-          let message2 = `Excellent! The price has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}price_settings.json`,
-            JSON.stringify({ price_euro: link }, null, "\t")
-          );
-
-          bot.sendMessage(chatId, message2);
+          fs.writeFileSync(`${pathToFolder}price_settings.json`, JSON.stringify({ price_euro: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The price has been saved in site`);
           break;
-        case "address":
-          let message3 = `Excellent! The address has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}address_settings.json`,
-            JSON.stringify({ address: link }, null, "\t")
-          );
 
-          bot.sendMessage(chatId, message3);
+        case "address":
+          fs.writeFileSync(`${pathToFolder}address_settings.json`, JSON.stringify({ address: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The address has been saved in site`);
           break;
 
         case "api-key":
-          let message4 = `Excellent! The api has been saved in site`;
-          fs.writeFileSync(
-            `${pathToFolder}api_key.json`,
-            JSON.stringify({ api_key: link }, null, "\t")
-          );
-
-          bot.sendMessage(chatId, message4);
+          fs.writeFileSync(`${pathToFolder}api_key.json`, JSON.stringify({ api_key: link }, null, "\t"));
+          bot.sendMessage(chatId, `Excellent! The api has been saved in site`);
           break;
 
         case "clear-users":
-          let message5 = `Excellent! The keys has been deleted`;
           fs.writeFileSync(`${pathToFolder}users-keys.json`, "[]");
-
-          bot.sendMessage(chatId, message5);
+          bot.sendMessage(chatId, `Excellent! The keys have been deleted`);
           break;
-        case "set_replain_id":
-          let message6 = `Excellent! The id has been saved`;
 
+        case "set_replain_id":
           fs.writeFileSync(
             `${pathToFolder}settings.json`,
             JSON.stringify({ ...oldSettings, replainId: link }, null, "\t")
           );
-
-          bot.sendMessage(chatId, message6);
+          bot.sendMessage(chatId, `Excellent! The id has been saved`);
           break;
 
         case "commission_precent":
-          let message8 = `Excellent! The precent has been saved`;
-
           fs.writeFileSync(
             `${pathToFolder}settings.json`,
             JSON.stringify({ ...oldSettings, commissionPrecent: link }, null, "\t")
           );
-
-          bot.sendMessage(chatId, message8);
+          bot.sendMessage(chatId, `Excellent! The percent has been saved`);
           break;
 
         case "additional-btc-price":
-          let message9 = `Excellent! The wallet transactions has been saved`;
-
-          const transaction_0 = link[0]
-          const transaction_1 = link[1]
-          const transaction_2 = link[2]
-
+          const transaction_0 = link[0];
+          const transaction_1 = link[1];
+          const transaction_2 = link[2];
           fs.writeFileSync(
             `${pathToFolder}transactions_wallet.json`,
             JSON.stringify({ transaction_0, transaction_1, transaction_2 }, null, "\t")
           );
-
-          bot.sendMessage(chatId, message9);
+          bot.sendMessage(chatId, `Excellent! The wallet transactions have been saved`);
           break;
 
-
         case "site_name":
-          let message7 = `Excellent! The siteName has been saved`;
           const capitalizedWord = link.charAt(0).toUpperCase() + link.slice(1);
           const toLower = link.toLowerCase();
-
-          const oldEmailSettings = JSON.parse(
-            fs.readFileSync(`${pathToFolder}email_settings.json`)
-          );
+          const oldEmailSettings = JSON.parse(fs.readFileSync(`${pathToFolder}email_settings.json`));
 
           fs.writeFileSync(
             `${pathToFolder}settings.json`,
@@ -220,16 +171,14 @@ module.exports.set_settings = (link, chatId, bot, step, site) => {
             )
           );
 
-          bot.sendMessage(chatId, message7);
+          bot.sendMessage(chatId, `Excellent! The siteName has been saved`);
           break;
+
         default:
           break;
       }
     } else {
-      bot.sendMessage(
-        chatId,
-        "You have entered an invalid site, please try again"
-      );
+      bot.sendMessage(chatId, "You have entered an invalid site, please try again");
     }
   });
 };
